e53a4bd4b78f612434315b4ae7d2fe9dfe4979f9
r750 <57615798+r750@users.noreply.github.com>
2022-05-23 22:55:27 +0530

    Add files via upload

--- SnakeNLadder.java
+++ SnakeNLadder.java
@@ -6,31 +6,29 @@ public class SnakeNLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder game.");
 
-        int Position = 0;
-        int newPosition = 0;
-        int winPositiom = 100;
+        int startPosition=0;
+        int Position=0;
+        int newPosition=0;
 
-        while (Position <= winPositiom) {
-            int DieRolled = (int) (Math.random() * 6) + 1;
-            int checkOption = (int) (Math.random() * 3);
+        int DieRolled = (int) (Math.random()*6)+1;
+        int checkOption = (int) (Math.random()*3);
+        //System.out.println(DieRolled);
 
-            switch (checkOption) {
-                case NO_PLAY:
-                    newPosition = 0;
-                    break;
-                case LADDER:
-                    newPosition = DieRolled;
-                    break;
-                case SNAKE:
-                    newPosition = -DieRolled;
-            }
-           // Position = Position + newPosition;
-            if (Position < 0) {
-                Position = 0;
-            }
-          
-            System.out.println("You are now at " + Position + "th" + " position");
+        switch (checkOption) {
+            case NO_PLAY:
+                newPosition = 0;
+                break;
+            case LADDER:
+                newPosition = DieRolled;
+                break;
+            case SNAKE:
+                newPosition = -DieRolled;
         }
+        Position=Position+newPosition;
+        if (Position<0){
+            Position = 0;
+        }
+        System.out.println("You are now at "+Position+"th"+" position");
     }
 }
 
