2088f64eb1e9e7a384d33d7673d15c47f448367a
r750 <57615798+r750@users.noreply.github.com>
2022-05-23 22:43:02 +0530

    Add files via upload

---
+++ SnakeNLadder.java
@@ -0,0 +1,9 @@
+public class SnakeNLadder {
+    
+        public static void main(String[] args) {
+            System.out.println("Welcome to Snake And Ladder game.");
+
+            int start=0;
+        }
+    }
+
